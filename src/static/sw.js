e7508215e1fa106e624642f3bc5cda2a928fe2ab
returntears <returntears@outlook.com>
2022-09-08 22:10:44 +0800

    网络层拦截

--- src/static/sw.js
+++ src/static/sw.js
@@ -2,5 +2,10 @@ self.addEventListener('install', event => {
   self.skipWaiting()
 })
 self.addEventListener('activate', event => {
-  // clients.claim()
+  clients.claim()
+})
+self.addEventListener('fetch', event => {
+  if (/network\.jpg$/.test(event.request.url)) {
+    return event.respondWith(fetch('./src/assets/pwa.jpg'))
+  }
 })
